8d9d156922f53c76e382ecea9eea32dcf9ce0f31
zhaojianzhi <377598262@qq.com>
2021-09-29 13:47:42 +0800

    删除冗余

--- src/main/java/com/zjz/mybatisTest/MybatisApplication.java
+++ src/main/java/com/zjz/mybatisTest/MybatisApplication.java
@@ -1,14 +1,5 @@
 package com.zjz.mybatisTest;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 /**
  * @author zjz
  * @date 2021/9/27 10:10
